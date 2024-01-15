45435d122dde66f9bede27967752692cf5b3cb61
JL-Asus_Graznov <graznov@me.com>
2024-01-15 11:48:34 +0300

    add FormInput

---
+++ src/Components/formInputs/formInputs.jsx
@@ -0,0 +1,14 @@
+import './formInputs.scss'
+function FormInputs(){
+
+    return(
+        <div className={'formInput'}>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+        </div>
+    )
+}
+
+export default FormInputs
---
+++ src/Container.jsx
@@ -0,0 +1,14 @@
+import './Container.scss'
+import FormInputs from "./Components/formInputs/formInputs.jsx";
+
+function Container() {
+
+  return (
+    <div className={'container'}>
+      <FormInputs/>
+    </div>
+  )
+}
+
+export default Container
+
