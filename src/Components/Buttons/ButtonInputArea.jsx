e2217a7ca171247cb073148faacfc5c3188f4ca5
JL-Asus_Graznov <graznov@me.com>
2024-01-15 19:22:29 +0300

    del all

--- src/Components/Buttons/ButtonInputArea.jsx
+++
@@ -1,8 +0,0 @@
-import './ButtonInputArea.scss'
-function ButtonInputArea({text}){
-    return(
-        <button>{text}</button>
-    )
-}
-
-export default ButtonInputArea
--- src/Container.jsx
+++ src/Container.jsx
@@ -1,11 +1,10 @@
 import './Container.scss'
-import FormInputs from "./Components/formInputs/formInputs.jsx";
 
 function Container() {
 
   return (
     <div className={'container'}>
-      <FormInputs/>
+
     </div>
   )
 }
