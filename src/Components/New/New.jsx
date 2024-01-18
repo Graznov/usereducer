38050f583d33d908f701e73d77b43504dbb9f6cc
JL-Huawei_Graznov <graznov@me.com>
2024-01-18 09:29:46 +0300

    New.jsx - completed

--- src/Components/New/New.jsx
+++ src/Components/New/New.jsx
@@ -1,8 +1,19 @@
 import './New.scss'
 import {useState} from "react";
+import classNames from "classnames";
 
 
 function New({onChange}){
+
+    const [inpBool, setInpBool] = useState(true)
+    const inputClass = classNames('abc',{
+        'abcRed' : !inpBool
+    })
+
+    const [btnBool, setBtnBool] = useState(true)
+    const btnClass = classNames('button',{
+        'btnDeactive' : !btnBool
+    })
 
     let [text, setText] = useState('')
     function inputVallue(event){
@@ -13,16 +24,26 @@ function New({onChange}){
         if (text) {
             onChange(text)
             setText('')
+        } else {
+            setText("Add text")
+            setInpBool(false)
+            setBtnBool(false)
+            setTimeout(()=>{
+                setText("")
+                setInpBool(true)
+                setBtnBool(true)
+            },1000)
+
         }
     }
 
     return(
         <div className="new">
             <div className="text">
-                <input onChange={inputVallue} className="abc" value={text}/>
+                <input onChange={inputVallue} className={inputClass} value={text}/>
             </div>
 
-            <button onClick={ClickPlus} className="NewWork">
+            <button onClick={ClickPlus} className={btnClass}>
 
                 <img src='https://cdn.icon-icons.com/icons2/404/PNG/128/plus_40632.png' alt="+"/>
 
